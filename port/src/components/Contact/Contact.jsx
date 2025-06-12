350836987d061b5fed3a95533f3eb595cfead604
Qwerty@126 <indrmy.126@gmail.com>
2025-06-12 13:10:45 +0530

    s

--- port/src/components/Contact/Contact.jsx
+++ port/src/components/Contact/Contact.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div></div>
+    <section
+    id='contact'
+    className='py-24 pb-24 px-[7vw] md:px-[7vw] lg:px-[18vw] font-sans'
+    >
+
+      <div className='text-center'>
+        <h2 className='text-white text-4xl font-bold'>CONTACT</h2>
+        <div className='w-32 h-1 bg-purple-500 mx-auto mt-4'></div>
+        <p className='text-gray-400 mt-4 text-lg font-semibold'>I’d love to hear from you—reach out for any opportunities or questions!</p>
+      </div>
+
+      <div className='text-center'>
+        <div></div>
+      </div>
+    </section>
   )
 }
 
